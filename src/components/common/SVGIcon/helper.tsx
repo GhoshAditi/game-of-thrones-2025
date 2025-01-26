bacce081daed38259fd3080cb2795060257a0895
GhoshAditi <hena.ghosh786@gmail.com>
2025-01-26 17:55:34 +0530

    Made the features mobile responsive

--- src/components/common/SVGIcon/helper.tsx
+++ src/components/common/SVGIcon/helper.tsx
@@ -1,9 +1,11 @@
 import { lazy } from 'react';
+import FootballPlayerSVG from './components/FootballPlayerSVG';
 
 export const IconMap = {
   leftHero: lazy(() => import('./components/LeftHeroSVG')),
   helmet: lazy(() => import('./components/HelmetSVG')),
   football: lazy(() => import('./components/FootballSVG')),
+
   sponsorsLines: lazy(() => import('./components/SponsorsLinesSVG')),
   logo: lazy(() => import('./components/LogoSVG')),
 };
