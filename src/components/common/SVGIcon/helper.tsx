6e0cd661687b34f36e5ff8af6ec3a16f79668a11
GhoshAditi <hena.ghosh786@gmail.com>
2025-01-26 18:06:22 +0530

    created components

--- src/components/common/SVGIcon/helper.tsx
+++ src/components/common/SVGIcon/helper.tsx
@@ -5,7 +5,6 @@ export const IconMap = {
   leftHero: lazy(() => import('./components/LeftHeroSVG')),
   helmet: lazy(() => import('./components/HelmetSVG')),
   football: lazy(() => import('./components/FootballSVG')),
-
   sponsorsLines: lazy(() => import('./components/SponsorsLinesSVG')),
   logo: lazy(() => import('./components/LogoSVG')),
 };
