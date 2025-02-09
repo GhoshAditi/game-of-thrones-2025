a46f2724e70f1ae49448112633cebc1dd0c415af
codewarnab <dp950810@gmail.com>
2025-02-09 20:08:20 +0530

    middlewere commented

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,24 +19,24 @@ export async function middleware(req: NextRequest) {
     return res;
   }
 
-  const { data: userRoles, error } = await supabase
-    .from('roles')
-    .select(`
-      role,
-      event_categories (
-        fest_name,
-        year
-      )
-    `)
-    .eq('id', session.user.id)
-    .eq('role', 'supar_admin')
-    .eq('event_categories.fest_name', 'Game Of Thrones')
-    .eq('event_categories.year', 2025);
+  // const { data: userRoles, error } = await supabase
+  //   .from('roles')
+  //   .select(`
+  //     role,
+  //     event_categories (
+  //       fest_name,
+  //       year
+  //     )
+  //   `)
+  //   .eq('id', session.user.id)
+  //   .eq('role', 'supar_admin')
+  //   .eq('event_categories.fest_name', 'Game Of Thrones')
+  //   .eq('event_categories.year', 2025);
 
-  // If an error occurs or no matching role is found, redirect away.
-  if (error || !userRoles || userRoles.length === 0) {
-    return NextResponse.redirect(new URL('/unauthorised', req.url));
-  }
+  // // If an error occurs or no matching role is found, redirect away.
+  // if (error || !userRoles || userRoles.length === 0) {
+  //   return NextResponse.redirect(new URL('/unauthorised', req.url));
+  // }
 
   return NextResponse.next();
 }
